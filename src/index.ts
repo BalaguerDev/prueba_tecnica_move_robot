cfb699765ae3978fde0563a19bcdcd364eda1638
abalaguer-webimpacto <abalaguer@webimpacto.es>
2023-09-19 13:33:05 +0200

    fix error in index

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import robotRutas from './routes/robotRutas';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use('/api/robot', robotRutas);
+
+// Ruta para manejar solicitudes a rutas no definidas
+app.use((req, res) => {
+  res.status(404).json({ error: 'Ruta no encontrada' });
+});
 
 app.use(errorHandler);
 
